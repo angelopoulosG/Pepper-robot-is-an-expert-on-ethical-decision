47711614b08b46876df16553e9f398123a664831
Georgios Angelopoulos <george.angelop@gmail.com>
2020-04-03 12:57:11 +0200

    Update thebrain.py

--- Server/thebrain.py
+++ Server/thebrain.py
@@ -5,75 +5,34 @@ Created on Thu Apr  2 16:01:44 2020
 @author: Georgios Angelopoulos
 """
 
-import select, socket, sys, Queue
+import socket
 
 
 
-HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 65432        # Port to listen on (non-privileged ports are > 1023)
 
-# Create a TCP/IP socket
-server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-server.setblocking(0)
-# Bind the socket to the port
-server_address = ('127.0.0.1', 10001)
+HOST = '192.168.1.14'  # Standard loopback interface address (localhost)
+PORT = 10001        # Port to listen on (non-privileged ports are > 1023)
 
-print >>sys.stderr, 'starting up on %s port %s' % server_address
-
-server.bind(server_address)
-# Listen for incoming connections
-server.listen(5)
-# Sockets from which we expect to read
-inputs = [ server ]
-# Sockets to which we expect to write
-outputs = [ ]
-# Outgoing message queues (socket:Queue)
-message_queues = {}
-
-
-
-while inputs:
-            
-    # Wait for at least one of the sockets to be ready for processing
-    #print >>sys.stderr, '\nwaiting for the next event'
-    readable, writable, exceptional = select.select(inputs, outputs, inputs)
-    # Handle inputs
-    for s in readable:
-        if s is server:
-            # A "readable" server socket is ready to accept a connection
-            connection, client_address = s.accept()
-            connection.setblocking(0)
-            inputs.append(connection)
-            # Give the connection a queue for data we want to send
-            message_queues[connection] = Queue.Queue()
-        else:
-            data=s.recv(1024)
-            if data:
-                if data == 'Ready':
-                    s.send("Begin")
-                else:
-                    print(data)
-                    if data == 'Begin OK':
-                        s.send("Camera")
-                    elif data == 'Camera OK':
-                        s.send("Hands")
-                    elif data == 'Hands OK':
-                        s.send("Stop")
-                    else:
-                        print("HI")
-
-
-                message_queues[s].put(data)
-                # Add output channel for response
-                if s not in outputs:
-                    outputs.append(s)
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))
+    s.listen()
+    conn, addr = s.accept()
+    with conn:
+        print('Connected by', addr)
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            if data == b'Ready':
+                conn.sendall(b"Begin")
             else:
-                # Interpret empty result as closed connection
-                # Stop listening for input on the connection
-                if s in outputs:
-                    outputs.remove(s)
-                inputs.remove(s)
-                s.close()
-
-                # Remove message queue
-                del message_queues[s]                
+                print(data)
+                if data == b'Begin OK':
+                    conn.sendall(b"Camera")
+                elif data == b'Camera OK':
+                    conn.sendall(b"Hands")
+                elif data == b'Hands OK':
+                    conn.sendall(b"Stop")
+                else:
+                    print("HI")
+            
