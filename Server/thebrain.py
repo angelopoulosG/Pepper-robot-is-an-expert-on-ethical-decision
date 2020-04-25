3ca56790d270b0fc748c0494c17c5493284ae504
Georgios Angelopoulos <george.angelop@gmail.com>
2020-04-25 12:55:34 +0200

    Update thebrain.py

--- Server/thebrain.py
+++ Server/thebrain.py
@@ -19,7 +19,7 @@ HOST = '192.168.1.14'  #you should change this
 ###################################################################################################
 ###################################################################################################
 ###################################################################################################
-def checklearning(pas, law, saving, swerve): 
+def checklearning(pas, law, saving, swerve):
     if pas == law:
         return '0'
     elif pas == saving:
@@ -34,29 +34,29 @@ def checklearning(pas, law, saving, swerve):
         return '5' 
     else:
         return 'ok'
-###################################################################################################      
+###################################################################################################
 def learningmore(pas, law, saving, swerve, answer):
     if pas == law:        
         if  answer == 'y':
             pas=pas+1
-            law=law-1                
+            law=law-1
         else:
             pas=pas-1
             law=law+1
-    elif pas == saving:        
+    elif pas == saving:
         if  answer == 'y':
             pas=pas+1
-            saving=saving-1                
+            saving=saving-1
         else:
             pas=pas-1
             saving=saving+1
     elif saving == law:
         if  answer == 'y':
             saving=saving-1
-            law=law+1           
+            law=law+1
         else:
             saving=saving+1
-            law=law-1      
+            law=law-1
     elif swerve == law:
         if  answer == 'y':
             swerve=swerve-1
@@ -79,21 +79,21 @@ def learningmore(pas, law, saving, swerve, answer):
             swerve=swerve+1
             pas=pas-1
     else:
-        return pas, law, saving, swerve  
+        return pas, law, saving, swerve
     return pas, law, saving, swerve
 ###################################################################################################
-def qrcode():     
-    image = cv2.imread('image.jpg')     
-    qrCodeDetector = cv2.QRCodeDetector()     
-    decodedText, points, _ = qrCodeDetector.detectAndDecode(image)     
-    if points is not None:     
-        nrOfPoints = len(points)     
+def qrcode():
+    image = cv2.imread('image.jpg')
+    qrCodeDetector = cv2.QRCodeDetector()
+    decodedText, points, _ = qrCodeDetector.detectAndDecode(image)
+    if points is not None:
+        nrOfPoints = len(points)
         for i in range(nrOfPoints):
             nextPointIndex = (i+1) % nrOfPoints
-            cv2.line(image, tuple(points[i][0]), tuple(points[nextPointIndex][0]), (255,0,0), 5)     
+            cv2.line(image, tuple(points[i][0]), tuple(points[nextPointIndex][0]), (255,0,0), 5)
         return decodedText
     else:
-        return 0     
+        return 0
 ###################################################################################################
 def speech_to_text():
     r = sr.Recognizer()
@@ -118,13 +118,13 @@ def speech_to_text():
     elif 'second' in message:
         message='second'
     else:
-        message = "silence"      
+        message = "silence"
     return  message 
-###################################################################################################        
-###################################################################################################        
-###################################################################################################        
-        
-        
+###################################################################################################
+###################################################################################################
+###################################################################################################
+
+
 
 PORT = 10001        # Port to listen on (non-privileged ports are > 1023)
 camera,audio,case,info= 0,0,0,0
@@ -149,16 +149,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
             elif data == b'Learning done':
                 conn.sendall(b"SendMeInfo.endmes")
                 audio=0
-                info=1     
+                info=1
                 learn=0
-                
-                
-                
+
+
+
             else:
                 ##############################################################
                 if audio == 1:
                     length=int(data)
-                    conn.sendall(b"ContinueWithVoice.endmes")                    
+                    conn.sendall(b"ContinueWithVoice.endmes")
                     audio = 2
                 elif audio == 2:
                     t = open("audio.wav", "ab+")
@@ -182,8 +182,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                             mystring= "BeginLearning.endmes" + message 
                             string = mystring.encode('utf-8')
                             conn.sendall(string)
-                            
-                            
+
+
                         if learn ==2 :
                             pas, law, saving, swerve =learningmore(pas, law, saving, swerve, message)
                             check = checklearning(pas, law, saving, swerve)
@@ -192,16 +192,16 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                                 string = mystring.encode('utf-8')
                                 conn.sendall(string)
                                 learn=2
-                            else:  
+                            else:
                                 print(pas, law, saving, swerve)
-                                conn.sendall(b"Stop.endmes")                            
-                            
-                ##############################################################                      
-                        
+                                conn.sendall(b"Stop.endmes")
+
+                ##############################################################
+
                 elif camera == 1:
                     length=int(data)
-                    conn.sendall(b"ContinueWithCamera.endmes")                    
-                    camera = 2         
+                    conn.sendall(b"ContinueWithCamera.endmes")
+                    camera = 2
                 elif camera == 2:
                     t = open("image.jpg", "ab+")
                     t.write(data)
@@ -209,7 +209,7 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                     bytes = os.stat('image.jpg').st_size
 
                     if bytes == length:
-                        camera = 0                   
+                        camera = 0
                         text=qrcode()
                         os.remove("image.jpg")
                         conn.sendall(b"Hands.endmes")
@@ -228,13 +228,9 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
                         learn=2
                         audio=1
                         info=0
-                    else: 
+                    else:
                         print(pas, law, saving, swerve)
                         conn.sendall(b"Stop.endmes")
-                        
+
                 else:
                     print("hi")
-               
-        
-
-            
